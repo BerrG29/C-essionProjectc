78d3fecd1686ae73cb351e63afef60cfda6d0860
Molomix <morgan.dhaese@gmail.com>
2018-01-22 19:07:28 +0100

    Optimisation de la méthode

--- src/lib.c
+++ src/lib.c
@@ -11,32 +11,19 @@ int comptage(char **matrice, char target, int indexLigne, int indexCol, int nbLi
 
     int i,j;
 
-    /*printf("Index de fin %d, %d\n",indexFinLig,indexFinCol);*/
-
-    //printf("Carac %c\n",target);
-
     for(i = indexDebLig; i <= indexFinLig; i++)
     {
         for(j = indexDebCol; j<=indexFinCol;j++)
         {
-            //printf("Matrice[%d][%d] = %c\n",i,j,matrice[i][j]);
-            //printf("Match ? %d\n",strcmp(&matrice[i][j],&target));
             if(j >= 0 && i >= 0 && i < nbLig && j < nbCol // Dans le cadre
                 && matrice[i][j] == target) // Ça match.
             {                
-                //if(i != indexLigne || j != indexCol)
-                { // On ne compare la case du milieu)
-                    //printf("Matrice[%d][%d] = %c\n",i,j,matrice[i][j]);
-                    compteur++;
-                } 
-                
+                    compteur++;                
             }   
         }
     }
 
     if(target == matrice[indexLigne][indexCol]){compteur--;}
-
-    /*printf("Compteur pour target : %c = %d\n",target,compteur);*/
 
     return compteur;
 }
