de91e3e55ad218ae077edd3f0d97b73a506dd084
Molomix <morgan.dhaese@gmail.com>
2018-01-22 16:16:22 +0100

    Début de l'algorithme de traitement

---
+++ src/lib.c
@@ -0,0 +1,42 @@
+int comptage(char **matrice, char target, int indexLigne, int indexCol, int nbLig, int nbCol, int distance){
+    int compteur = 0;
+    
+    int indexDebLig, indexDebCol, indexFinLig, indexFinCol;
+    
+    indexDebLig = indexLigne - distance;
+    indexFinLig = indexLigne + distance;
+
+    indexDebCol = indexCol - distance;
+    indexFinCol = indexCol + distance;
+
+    int i,j;
+
+    printf("Index de fin %d, %d\n",indexFinLig,indexFinCol);
+
+    //printf("Carac %c\n",target);
+
+    for(i = indexDebLig; i <= indexFinLig; i++)
+    {
+        for(j = indexDebCol; j<=indexFinCol;j++)
+        {
+            //printf("Matrice[%d][%d] = %c\n",i,j,matrice[i][j]);
+            //printf("Match ? %d\n",strcmp(&matrice[i][j],&target));
+            if(j >= 0 && i >= 0 && i < nbLig && j < nbCol // Dans le cadre
+                && matrice[i][j] == target) // Ça match.
+            {                
+                //if(i != indexLigne || j != indexCol)
+                { // On ne compare la case du milieu)
+                    //printf("Matrice[%d][%d] = %c\n",i,j,matrice[i][j]);
+                    compteur++;
+                } 
+                
+            }   
+        }
+    }
+
+    if(target == matrice[indexLigne][indexCol]){compteur--;}
+
+    printf("Compteur pour target : %c = %d\n",target,compteur);
+
+    return compteur;
+}
--- src/traitement.c
+++ src/traitement.c
@@ -1,16 +1,27 @@
+/**
+ * 
+ * Pour lancer le traitement, il suffit d'exécuter : 
+ * gcc -Wall traitement.c -o a.out && ./a.out
+ * 
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "lib.c"
 
 int main(int argc, char *argv[])
 {
     char **matrice;
-    matrice = (char **) malloc(4 * sizeof(char *));
+    int nbLigne = 4, nbColonne = 4;
+    matrice = (char **) malloc(nbLigne * sizeof(char *));
     //matrice = (char **) calloc(4, sizeof(char *));
 
     int i, j;
-    for(i=0; i<4; i++)
+    for(i=0; i<nbLigne; i++)
     {
-        matrice[i] = (char*) malloc(4 * sizeof(char));
+        matrice[i] = (char*) malloc(nbColonne * sizeof(char));
         //matrice[i] = (char*) calloc(4,sizeof(char));
     }
 
@@ -28,6 +39,11 @@ int main(int argc, char *argv[])
         }
     }*/
 
+    // matrice[0] = "XXXX";
+    // matrice[1] = "XX0X";
+    // matrice[2] = "XXXX";
+    // matrice[3] = "XAXX";
+
     matrice[0][0] = 'X';
     matrice[0][1] = 'X';
     matrice[0][2] = 'X';
@@ -35,7 +51,7 @@ int main(int argc, char *argv[])
 
     matrice[1][0] = 'X';
     matrice[1][1] = 'X';
-    matrice[1][2] = 'O';
+    matrice[1][2] = 'X';
     matrice[1][3] = 'X';
 
     matrice[2][0] = 'X';
@@ -50,9 +66,9 @@ int main(int argc, char *argv[])
 
 
     // Affichage matrice
-    for(i=0; i<4; i++)
+    for(i=0; i<nbLigne; i++)
     {
-        for(j=0; j<4; j++)
+        for(j=0; j<nbColonne; j++)
         {
             printf("%c",matrice[i][j]);
         }
@@ -63,11 +79,59 @@ int main(int argc, char *argv[])
 
     // Établissement règles
 
+    char **conditions;
+    int nbConditions = 3,nbCaracteres = 11;
+
+    conditions = (char **) malloc(nbConditions * sizeof(char *));
+
+    for(int i = 0; i < nbConditions; i++){
+        conditions[i] = (char *) malloc(nbCaracteres * sizeof(char));
+    }
+
+
+    // Initialisation des conditions
+    conditions[0] = "fdsfsdf";
+
+    int len;
+    printf("Condition 1 : '%s'\n",conditions[0]);
+    printf("Condition 1 - 2 : '%c'\n",conditions[0][1]);
+
+
+    conditions[0] = "X-1O1O";  
+    conditions[1] = "0=8X1X";
+    conditions[2] = "A-1X1-1O1X";
+
+    len = strlen(conditions[0]);
+    printf("Length 1 : '%d'\n",len);  
+
+    // Traitement
+
+    int cpt;
+
+    printf("\n#-----------------------#\n");
+    printf("#   Début traitement    #\n");
+    printf("#-----------------------#\n\n");
+
+    for(i = 0;i< nbLigne;i++){
+        for(j=0;j<nbColonne;j++){
+
+            if(matrice[i][j] == conditions[0][0]){
+                cpt= comptage(matrice,
+                            conditions[0][0],
+                            i, j,nbLigne,nbColonne,
+                            atoi(&conditions[0][4]));
+                printf("---------------\n");
+            }
+        }
+    }
 
 
 
+    printf("Distance %d\n",atoi(&conditions[0][4]));
 
+    printf("Comptage = %d\n",cpt);
 
+    // Désallocation de la mémoire
 
     for(i=0; i<4; i++)
     {
@@ -76,8 +140,20 @@ int main(int argc, char *argv[])
 
     free(matrice);
 
-
-
     return 0;
 
+}
+
+int testCaseAutour(char**matrice, int indexLig, int indexCol, int nbCaseAutour, int nbLig, int nbCol, char * condition){
+    int i, j;
+
+    int indexDebLig, indexDebCol, indexFinLig, indexFinCol;
+    indexDebLig = index - nbCaseAutour;
+
+
+    for(i = 0; i<nbLig; i++){
+        for(j = 0; j<nbCol;j++){
+            
+        }
+    }
 }
