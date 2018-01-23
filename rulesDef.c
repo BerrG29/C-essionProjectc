35d6747c9c6f3dca242f2bf1deae3b96b4bf00d7
berrou <glennberr29@gmail.com>
2018-01-23 09:31:51 +0100

    interface pour creer les rules et affichage

--- rulesDef.c
+++ rulesDef.c
@@ -3,79 +3,96 @@
 #include <string.h>
 
 /* function returning some rules requested by a user */
-char condition() {
+char **rule() {
 
-   char conditions[100][100];
+   //char conditions[100][100];
    char newRule='d';
    int nbConditions;
    int cpt;
    int ligne=0;
    int colonne=0;
+   char **rules = (char **) malloc(sizeof(char *) * 100);/* ou malloc(L * sizeof *A); */
+    for (int i = 0 ; i < 100 ; i++)
+    {
+        rules[i] = (char *) malloc(sizeof(char)*100);
+    }
    
+   //new rule
    while(newRule!='N'){
 	 cpt=1;
-
+	 char *p;
          printf("Entrer un caractère :\n");
-         scanf(" %c",&conditions[ligne][colonne]);
+	 //scanf("%c", p);
+         scanf(" %c",&rules[ligne][colonne]);
 	 colonne++;
 
 	 printf("Combien voulez vous de conditions?\n");
 	 scanf("%d",&nbConditions);
 
+	 //new condition
 	 while (cpt<=nbConditions){
 		printf("Condition %d\n",cpt);
 		printf("S'il y a au moins(-), au plus(+), aucun(0), exactement(=) ?\n");
-		scanf(" %c",&conditions[ligne][colonne]);
+		scanf(" %c",&rules[ligne][colonne]);
 		colonne++;
-		if (conditions[ligne][colonne-1]=='-' || conditions[ligne][colonne-1]=='+'){
+		if (rules[ligne][colonne-1]=='-' || rules[ligne][colonne-1]=='+'){
 			printf("Combien?\n");
-			scanf(" %c",&conditions[ligne][colonne]);
+			scanf(" %c",&rules[ligne][colonne]);
 			colonne++;
 		}
 		printf("Caractère de type?\n");
-		scanf(" %c",&conditions[ligne][colonne]);
+		scanf(" %c",&rules[ligne][colonne]);
 		colonne++;
 		printf("A quelle distance?\n");
-		scanf(" %c",&conditions[ligne][colonne]);
+		scanf(" %c",&rules[ligne][colonne]);
 		colonne++;
          	if (nbConditions-cpt>=1){
-			printf("Structure logique entre les contitions? (& = ET/ | = OU)\n");
-			scanf(" %c",&conditions[ligne][colonne]);
+			printf("Structure logique entre les contitions? (& = ET / | = OU)\n");
+			scanf(" %c",&rules[ligne][colonne]);
 			colonne++;
 		}
-		conditions[ligne][colonne]='/';
 		cpt++;
 	 }
+
+	 printf("la case devient de type? \n");
+	 scanf(" %c",&rules[ligne][colonne]);
+         colonne++;
+	 rules[ligne][colonne]='/';
+
          printf("Voulez vous entrer une autre règle (O/N)?\n");
          scanf(" %c",&newRule);
+
 	 ligne++;
 	 colonne=0; 
    }
-   conditions[ligne][colonne]='/';
+   rules[ligne][colonne]='/';
+
+   return rules; 
+}
+
+void printRes(char **rules){
+   //priting
    int i=0;
    int j=0;
  
-   while(i!='/') {
-        while(j!='/') {
-            //printf(" %c", conditions[i][j]);
+   while(rules[i][j]!='/') {
+        while(rules[i][j]!='/') {
+	    printf("%c ", rules[i][j]);
 	    j++;
-	    printf("%d ", j);
         }
+	printf("\n");
 	i++;
-        printf("%d", i);
-        printf("\n");
+	j=0;
     }
-   return conditions; 
 }
 
 
 
-int main(int argc, char *argv[])
-{
-    condition();
-    return EXIT_SUCCESS;
-
-}
+//int main(int argc, char *argv[])
+//{
+//    printRes(rule());
+//    return EXIT_SUCCESS;
+//}
 
 
 
